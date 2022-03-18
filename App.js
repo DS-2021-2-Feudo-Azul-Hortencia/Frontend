ade9f742bc6572face7bf37f3da6ce11fbf05418
Joberth Rogers <joberth.rogers18@gmail.com>
2022-03-17 23:13:37 -0400

    arrumando depedencias para roteament

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/pages/login';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,22 +9,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-<<<<<<< Updated upstream
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
-=======
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name="Login"
           component={Login}
-          options={{ title: 'Welcome' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
->>>>>>> Stashed changes
   );
 }
 
