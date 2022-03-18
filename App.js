c637742c0e083d8d2d47c55cd6a537cd919804ed
Leonardo Gonçalves Machado <89925347+leonardogonmac@users.noreply.github.com>
2022-03-17 22:31:39 -0300

    Ajustando as pastas no projeto

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
   return (
+<<<<<<< Updated upstream
     <View style={styles.container}>
       <Text>Hello!</Text>
       <StatusBar style="auto" />
     </View>
+=======
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+>>>>>>> Stashed changes
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
